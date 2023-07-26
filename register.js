c03032e8328c8a788abbb480dc539142b7d7a4c2
Stephenasante21 <stephen.asante@student.ehb.be>
2023-07-27 00:13:15 +0200

    login

--- register.js
+++ register.js
@@ -2,13 +2,20 @@ document.getElementById('form').addEventListener("submit", event => {
     event.preventDefault();
 
     let user = {}
+    user.username = document.getElementById("username")
     user.email = document.getElementById("email").value;
     user.password = document.getElementById("password").value;
+    user.password2 = document.getElementById("password2").value;
 
-    //check for login 
-    getData("http://localhost:3000/login", "POST", user).then(data => {
-        console.log(data)
-    })
+    //check for the password
+    if(user.password == user.password2){
+        getData("http://localhost:3000/register", "POST", user).then(data => {
+            alert(data.message)
+        })
+    }else{
+        alert("Passwords does not match!!!")
+    }
+
 })
 
 async function getData(url, method, data){
